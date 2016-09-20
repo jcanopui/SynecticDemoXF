ecc93bf691d4d7d063f40cf2c18d941d74519b85
Oleguer Bardalet Laguna <obardale@everis.com>
2016-09-20 16:02:26 +0200

    Now all fields returned by the API call are shown. Some minor bugfixes.

--- app/pages/networking/networking.ts
+++ app/pages/networking/networking.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import { reorderArray } from 'ionic-angular';
+import { Platform, reorderArray } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -11,7 +11,10 @@ export class BasicPage {
   
 	posts: any;
 
-  constructor(private http: Http) {}
+  constructor(
+    public platform: Platform,
+    private http: Http
+  ) {}
 
   post(url, data) {
     return this.http.post(url, data);
@@ -19,7 +22,15 @@ export class BasicPage {
 
   makeNetworkCall() {
 
-    let url = 'https://d4w69355hi.execute-api.us-east-1.amazonaws.com/DEV/keyvalue';
+    let url = '';
+
+    if(this.platform.is('mobile')) {
+      url = 'https://d4w69355hi.execute-api.us-east-1.amazonaws.com/DEV/keyvalue';
+    }
+    else {
+      url = '/DEV/keyvalue';
+    }
+
     this.post(url,{}).map(res => res.json()).subscribe(data => {
             this.posts = data.values;
         }, error => {
